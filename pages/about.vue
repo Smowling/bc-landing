db491b5cdea47227e12d78fea9cad78aed44e7cd
Daniel Smółka <smowling@gmail.com>
2024-03-08 15:15:24 +0100

    locales

--- pages/about.vue
+++ pages/about.vue
@@ -11,12 +11,12 @@ definePageMeta({
     <main class="grid lg:grid-cols-2 place-items-center pb-4 ">
       <div>
       <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight mt-4">
-        What is it all about
+        {{ $t('about_header') }}
       </h1>
       <p class="text-lg mt-4 text-slate-600 max-w-xl text-justify">
-        Hi, I'm Daniel and I hate to leave my bike at LBS for weeks to gather dust and wait for a fix. So... I'm working on Bikecheck!<br />
-        Thats the main reason, but the scope grew. Now I want to create a platform that connects bikeshops with customers and smooths the expirience for both sides.<br />
-        For the moment its but a landing page, to gather leads and ideas, feel free to let me know if you have any.
+        {{ $t('about_story1')}}<br />
+        {{ $t('about_story2')}}<br />
+        {{ $t('about_story3')}}
       </p>
     </div>
 
