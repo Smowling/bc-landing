ec6b1c64faf14d4b65d5a73561b8e4490b1c08c7
Daniel Smółka <smowling@gmail.com>
2024-03-09 15:52:47 +0100

    cta translation

--- components/landing/Cta.vue
+++ components/landing/Cta.vue
@@ -2,9 +2,9 @@
   <div
     class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
   >
-    <h2 class="text-white text-3xl md:text-6xl pb-6">Do you want to know more?</h2>
+    <h2 class="text-white text-3xl md:text-6xl pb-6">{{ $t('cta_header') }}</h2>
     <div class="flex mt-5">
-      <LandingLink href="/contact" styleName="inverted">Contact me.</LandingLink>
+      <LandingLink href="/contact" styleName="inverted">{{ $t('cta_text') }}</LandingLink>
     </div>
   </div>
 </template>
