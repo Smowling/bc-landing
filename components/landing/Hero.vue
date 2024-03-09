b30d37a53dd322e5c2b6a7638977f7fcf984ba78
Daniel Smółka <smowling@gmail.com>
2024-03-09 14:47:00 +0100

    hero translation

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -15,11 +15,10 @@
     <div>
       <SlidingText />
       <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight mt-4">
-        Book and service your bike with us
+        {{ $t('hero_header') }}
       </h1>
       <p class="text-lg mt-4 text-slate-600 max-w-xl text-justify">
-        Bikecheck! is a website that will help you find local bike service, book and service your bike.
-        We are here so you could be on your bike more.
+        {{ $t('hero_desc') }}
       </p>
     </div>
   </main>
